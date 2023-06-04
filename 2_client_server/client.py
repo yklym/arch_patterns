05848f76ec9b751a1206512741d383d5e1d7e9de
yklym <yaroslaw.klymko@gmail.com>
2023-06-04 22:08:32 +0300

    finished client server

---
+++ 2_client_server/client.py
@@ -0,0 +1,25 @@
+import urllib.request
+import json
+
+
+def get_sound():
+    url = 'http://localhost:3000/get_sound'
+    response = urllib.request.urlopen(url)
+    return response.read().decode()
+
+
+def post_message(new_sound):
+    url = 'http://localhost:3000/set_sound'
+    data = {'sound': new_sound}
+    data = json.dumps(data).encode()
+    req = urllib.request.Request(url, data=data, headers={
+                                 'Content-Type': 'application/json'})
+    response = urllib.request.urlopen(req)
+
+
+if __name__ == '__main__':
+    while True:
+        new_sound = input('New sound or enter: ').strip()
+        if new_sound:
+            post_message(new_sound)
+        print('Curr sound:', get_sound())
---
+++ 2_client_server/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+curr_animal_sound = 'meow'
+
+
+@app.route('/get_sound', methods=['GET'])
+def get_sound():
+    return curr_animal_sound
+
+
+@app.route('/set_sound', methods=['POST'])
+def set_sound():
+    global curr_animal_sound
+    new_sound = request.json['sound']
+    curr_animal_sound = new_sound
+    print('new sound', new_sound)
+    return {'ok': 'ok'}
+
+
+if __name__ == '__main__':
+    app.run(port=3000)
